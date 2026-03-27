33d7716e6b42acba2108aef904fa0776229ef7ee
colinchuTaiwan <166598203+colinchuTaiwan@users.noreply.github.com>
2026-03-28 00:06:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,7 +141,7 @@ if "v10" not in st.session_state:
 
 v = st.session_state.v10
 now = now_taipei()
-SCHEDULE = ["08:40", "9:30", "10:50", "12:20", "13:15", "15:00", "00:04"]
+SCHEDULE = ["08:40", "9:30", "10:50", "12:20", "13:15", "15:00", "00:10"]
 
 # 🔒 穩定排程點判定
 current_slot_key = ""
